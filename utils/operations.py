8c09c93f5e868404671d69d1febfca4cbabe6687
al386152 <al386152@uji.es>
2024-09-18 19:22:43 +0200

    Cambio en el nombre de los pesos del modelo

--- utils/operations.py
+++ utils/operations.py
@@ -10,7 +10,7 @@ import utils.constants as cons
 fecha = datetime.now().replace(microsecond=0)
 
 def OUTPUT_MODEL_NAME(name:str, number_clases:int)->str:
-    return f"model_{name}_{number_clases}_outputs.pth"
+    return f"model_{name}_{number_clases}_outputs_{fecha}.pth"
 # -- Fin OUTPUT_MODEL_NAME -- #
 
 # Se asume que la lista no está vacía
