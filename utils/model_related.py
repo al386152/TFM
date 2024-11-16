6f7e0549fac6c6a07ef66a020c9fcf3af5e28f06
al386152 <al386152@uji.es>
2024-11-16 16:04:06 +0100

    Corrección de un fallo inesperado en el que, por alguna razón, pueden establecerse clases inexistentes cuando se "redondea" el resultado

--- utils/model_related.py
+++ utils/model_related.py
@@ -120,11 +120,14 @@ def from_regression_to_classification(outputs:torch.Tensor, boundaries:torch.Ten
     # Se ponen los datos en la clase que les tocaría
     outputs = torch.bucketize(input=outputs, boundaries=boundaries)    
     outputs = outputs.to(torch.int)
+    # Al parecer, hay algún caso en el que se pasa a una clase inexistente
+    outputs.apply_(lambda x: (x if x < num_classes else num_classes-1) )
     #print(f"outputs - post | type: {outputs.dtype} |\n{outputs}")
 
     # Se preparan los datos en el formato esperado para las métricas.
     addapted_output = list()
 
+    print("ouput[0]: {ouput[0]}")
     for ouput in outputs:        
         mod_ouput = [0] * num_classes
         mod_ouput[ouput[0]] = 1
